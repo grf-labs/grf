c9e4a04b742e309714b2a4466863831c8b8a9402
Marvin Wright <wright@imbs.uni-luebeck.de>
2014-10-23 17:56:55 +0200

    update windows version

--- test/test_survival.R
+++ test/test_survival.R
@@ -23,3 +23,10 @@ boxplot(result)
 
 ##ranger(Surv(time, status) ~ age + sex + perfor + surg + etype, colon)
 ##ranger(Surv(time, status) ~ age + sex + perfor + surg + etype, colon, splitrule = "auc")
+
+x <- seq(min(veteran$time),  max(veteran$time), length = 100)
+
+hist(veteran$time, 50, probability = TRUE)
+lines(density(veteran$time), col = "red")
+lines(x, dweibull(x, shape = 1, scale = 100), col = "blue")
+
