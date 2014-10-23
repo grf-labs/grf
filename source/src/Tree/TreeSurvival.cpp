df3415147f06f931172e5317c655b79bde375488
Marvin Wright <wright@imbs.uni-luebeck.de>
2014-10-23 20:47:01 +0200

    fix logrank problem

--- source/src/Tree/TreeSurvival.cpp
+++ source/src/Tree/TreeSurvival.cpp
@@ -153,7 +153,7 @@ void TreeSurvival::findBestSplit(size_t nodeID, std::vector<size_t>& possible_sp
     data->getAllValues(possible_split_values, sampleIDs[nodeID], varID);
 
     // Try next variable if all equal for this
-    if (possible_split_values.size() < 2) {
+    if (possible_split_values.size() == 0) {
       continue;
     }
 
