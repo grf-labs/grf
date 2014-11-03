435c6b941c0260177074c1cb02a4f1e11fb50c51
Marvin Wright <wright@imbs.uni-luebeck.de>
2014-11-03 15:37:39 +0100

    add new test function and data

---
+++ test/profile_survival.R
@@ -0,0 +1,29 @@
+
+library(survival)
+library(ranger)
+library(randomForestSRC)
+library(microbenchmark)
+
+ntree <- 50
+mtry <- 3
+nodesize <- 10
+formula <- Surv(time, status) ~.
+
+n <- 500
+p <- 6
+beta <- c(1,0.5,0.1,0,1,2)
+
+x <- matrix(rnorm(p*n, 1, 5), nrow = n)
+time <- x %*% beta + rnorm(n, 0, 0.5)
+time <- time - min(time)
+status <- rbinom(n, 1, 0.7)
+dat <- data.frame(time, status, x)
+
+microbenchmark(
+  Ranger = ranger(formula, dat, num.trees = ntree, mtry = mtry, min.node.size = nodesize), 
+  SRC = rfsrc(formula, dat, ntree = ntree, mtry = mtry, nodesize = nodesize), 
+  times = 1)
+
+
+rg = ranger(formula, dat, num.trees = ntree, mtry = mtry, min.node.size = nodesize, write.forest = TRUE)
+src = rfsrc(formula, dat, ntree = ntree, mtry = mtry, nodesize = nodesize)
