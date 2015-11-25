ef6b515773db923c6f5243cfd52d2a96a71bcfcc
Marvin Wright <wright@imbs.uni-luebeck.de>
2015-11-25 08:33:51 +0100

    reduce number of trees for csrf tests

--- ranger-r-package/ranger/tests/testthat/test_csrf.R
+++ ranger-r-package/ranger/tests/testthat/test_csrf.R
@@ -9,11 +9,11 @@ test_that("csrf classification returns predictions", {
   iris.test <- iris[-train.idx, ]
   
   pred <- csrf(Species ~ ., training_data = iris.train, test_data = iris.test, 
-               params1 = list(num.trees = 50), 
-               params2 = list(num.trees = 10))
+               params1 = list(num.trees = 10), 
+               params2 = list(num.trees = 3))
   
   expect_that(pred, is_a("factor"))
-  expect_that(length(pred), equals(50))
+  expect_that(length(pred), equals(nrow(iris)/3))
 })
 
 test_that("csrf regression returns predictions", {
@@ -22,9 +22,9 @@ test_that("csrf regression returns predictions", {
   iris.test <- iris[-train.idx, ]
   
   pred <- csrf(Sepal.Length ~ ., training_data = iris.train, test_data = iris.test, 
-               params1 = list(num.trees = 50), 
-               params2 = list(num.trees = 10))
+               params1 = list(num.trees = 10), 
+               params2 = list(num.trees = 3))
   
   expect_that(pred, is_a("numeric"))
-  expect_that(length(pred), equals(50))
+  expect_that(length(pred), equals(nrow(iris)/3))
 })
