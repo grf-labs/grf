08083640e7fbc77ddb59aa667496e64b71c9574d
Marvin Wright <wright@imbs.uni-luebeck.de>
2016-02-08 11:41:51 +0100

    reproduce issue #36

--- ranger-r-package/ranger/tests/testthat/test_ranger.R
+++ ranger-r-package/ranger/tests/testthat/test_ranger.R
@@ -535,3 +535,25 @@ test_that("No error if survival tree without OOB observations", {
               not(throws_error()))
 })
 
+test_that("as.factor() in formula works", {
+  n <- 20
+  dt <- data.frame(x = runif(n), y = rbinom(n, 1, 0.5))
+  expect_that(ranger(as.factor(y) ~ ., data = dt, num.trees = 5, write.forest = TRUE), 
+              not(throws_error()))
+})
+
+test_that("If respect.unordered.factors=TRUE, regard characters as unordered", {
+  n <- 20
+  dt <- data.frame(x = sample(c("A", "B", "C", "D"), n, replace = TRUE), 
+                   y = rbinom(n, 1, 0.5), 
+                   stringsAsFactors = FALSE)
+  
+  set.seed(2)
+  rf.char <- ranger(y ~ ., data = dt, num.trees = 5, min.node.size = n/2, respect.unordered.factors = TRUE)
+  
+  dt$x <- factor(dt$x, ordered = FALSE)
+  set.seed(2)
+  rf.fac <- ranger(y ~ ., data = dt, num.trees = 5, min.node.size = n/2, respect.unordered.factors = TRUE)
+  
+  expect_that(rf.char$prediction.error, equals(rf.fac$prediction.error))
+})
