c97a34ec066d2283d05d8bf609fbcaf2240f8d6c
animusnaturae <bjoernhergen.laabs@student.uni-luebeck.de>
2016-04-05 20:43:54 +0200

    Test structure improved

---
+++ ranger-r-package/ranger/tests/testthat/test_ranger_imp.R
@@ -0,0 +1,34 @@
+##This skript provides the tests for importance measures
+
+library(ranger)
+library(survival)
+context("ranger")
+
+##Initialize the random forests
+
+rg.imp <- ranger(Species ~ ., data = iris, verbose = FALSE, write.forest = TRUE,
+                 importance = "impurity")
+rg.perm <- ranger(Species ~ ., data = iris, verbose = FALSE, write.forest = TRUE,
+                 importance = "permutation")
+rg.scale.perm <- ranger(Species ~ ., data = iris, verbose = FALSE, write.forest = TRUE,
+                 importance = "permutation", scale.permutation.importance = TRUE)
+
+##Tests
+test_that("importance measures work", {
+  expect_that(rg.imp$variable.importance, is_a("numeric"))
+  expect_that(rg.perm$variable.importance, is_a("numeric"))
+  expect_that(rg.scale.perm$variable.importance, is_a("numeric"))
+})
+
+test_that("gini importance is larger than 1", {
+  expect_that(rg.imp$variable.importance[1], is_more_than(1))
+})
+
+test_that("unscaled importance is smaller than 1", {
+  expect_that(rg.perm$variable.importance[1], is_less_than(1))
+})
+
+test_that("scaled importance is larger than 1", {
+  expect_that(rg.scale.perm$variable.importance[1], is_more_than(1))
+})
+
---
+++ ranger-r-package/ranger/tests/testthat/test_ranger_surv.R
@@ -0,0 +1,56 @@
+##This skript provides the tests for random forests for survival analysis
+
+library(ranger)
+library(survival)
+context("ranger")
+
+##Initialize the random forest for survival analysis
+rg.surv <- ranger(Surv(time, status) ~ ., data = veteran, verbose = FALSE, write.forest = TRUE)
+
+##Basic tests (for all random forests equal)
+test_that("survival result is of class ranger with 15 elements", {
+  expect_that(rg.surv, is_a("ranger"))
+  expect_that(length(rg.surv), equals(15))
+})
+
+test_that("results have 500 trees", {
+  expect_that(rg.surv$num.trees, equals(500))
+})
+
+test_that("results have right number of independent variables", {
+  expect_that(rg.surv$num.independent.variables, equals(ncol(veteran) - 2))
+})
+
+test_that("Alternative interface works for survival", {
+  rf <- ranger(dependent.variable.name = "time", status.variable.name = "status", data = veteran)
+  expect_that(rf$treetype, equals("Survival"))
+})
+
+test_that("Matrix interface works for survival", {
+  rf <- ranger(dependent.variable.name = "time", status.variable.name = "status", data = data.matrix(veteran), write.forest = TRUE)
+  expect_that(rf$treetype, equals("Survival"))
+  expect_that(rf$forest$independent.variable.names, equals(colnames(veteran)[c(1:2, 5:8)]))
+})
+
+test_that("Matrix interface prediction works for survival", {
+  dat <- data.matrix(veteran)
+  rf <- ranger(dependent.variable.name = "time", status.variable.name = "status", data = dat, write.forest = TRUE)
+  expect_that(predict(rf, dat), not(throws_error()))
+})
+
+test_that("predict works for single observations, survival", {
+  rf <- ranger(Surv(time, status) ~ ., veteran, write.forest = TRUE)
+  pred <- predict(rf, head(veteran, 1))
+  expect_that(length(pred$survival), equals(length(rf$unique.death.times)))
+})
+
+##Special tests for random forests for survival analysis
+test_that("unique death times in survival result is right", {
+  expect_that(rg.surv$unique.death.times, equals(sort(unique(veteran$time))))
+})
+
+test_that("C-index splitting works", {
+  rf <- ranger(Surv(time, status) ~ ., data = veteran, verbose = FALSE, 
+               splitrule = "C")
+  expect_that(rf$treetype, equals("Survival"))
+})
