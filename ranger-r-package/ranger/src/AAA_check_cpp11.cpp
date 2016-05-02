080856aa45ed86a44f7b65fb42a49c3f7bd001c1
Marvin Wright <wright@imbs.uni-luebeck.de>
2016-05-02 08:04:12 +0200

    introduce NEW_WIN_R_BUILD and OLD_WIN_R_BUILD

--- ranger-r-package/ranger/src/AAA_check_cpp11.cpp
+++ ranger-r-package/ranger/src/AAA_check_cpp11.cpp
@@ -1,7 +1,13 @@
 
-#ifndef WIN_R_BUILD
-#if __cplusplus < 201103L
-#error Error: ranger requires a real C++11 compiler. You probably have to update gcc. 
-#endif
+#ifdef WIN_R_BUILD
+  #if __cplusplus < 201103L
+    #define OLD_WIN_R_BUILD
+  #else 
+    #define NEW_WIN_R_BUILD
+  #endif
+#else
+  #if __cplusplus < 201103L
+    #error Error: ranger requires a real C++11 compiler. You probably have to update gcc. 
+  #endif
 #endif
 
