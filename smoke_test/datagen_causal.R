542b328c11fe5af4c46e5a9699cf35aa954a0880
Stefan Wager <swager@stanford.edu>
2016-08-15 19:35:58 +0300

    causal test

---
+++ smoke_test/datagen_causal.R
@@ -0,0 +1,9 @@
+p = 10
+n = 1000
+X = matrix(round(2 * runif(n * p) - 1, 2), n, p)
+W = rbinom(n, 1, 1/(1 + exp(X[,3])))
+Y = round((2*W - 1) * (X[,1] > 0) + X[,2] + rnorm(n), 2)
+
+D = data.frame(X=X, Y=Y, W=W)
+
+write.table(D, file="smoke_test/causal.dat", row.names=FALSE)
