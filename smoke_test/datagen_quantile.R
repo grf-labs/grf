caf319a7e40e868386ea34668476021371ae2c08
Julie Tibshirani <julietibs@gmail.com>
2016-12-20 18:06:18 -0500

    More clean-up in the repo structure.

--- smoke_test/datagen_quantile.R
+++
@@ -1,8 +0,0 @@
-p = 10
-n = 1000
-X = matrix(round(2 * runif(n * p) - 1, 2), n, p)
-Y = (2 * rbinom(n, 1, 0.5) - 1) * (1 + 9 * (X[,1] > 0))
-
-D = data.frame(X=X, Y=Y)
-
-write.table(D, file="smoke_test/quantile.dat", row.names=FALSE)
