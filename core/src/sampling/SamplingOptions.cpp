474e956ad9dbc3f1c9ea02290bd8028da5d824a1
Julie Tibshirani <julietibs@gmail.com>
2016-12-24 11:31:40 -0800

    Add keep_in_bag to SamplingOptions.

--- core/src/sampling/SamplingOptions.cpp
+++ core/src/sampling/SamplingOptions.cpp
@@ -2,10 +2,12 @@
 
 SamplingOptions::SamplingOptions(bool sample_with_replacement,
                                  double sample_fraction,
-                                 std::string case_weights_file):
+                                 std::vector<double>* case_weights,
+                                 bool keep_in_bag):
     sample_with_replacement(sample_with_replacement),
     sample_fraction(sample_fraction),
-    case_weights_file(case_weights_file) {}
+    case_weights(case_weights),
+    keep_in_bag(keep_in_bag) {}
 
 bool SamplingOptions::get_sample_with_replacement() const {
   return sample_with_replacement;
@@ -15,6 +17,10 @@ double SamplingOptions::get_sample_fraction() const {
   return sample_fraction;
 }
 
-const std::string &SamplingOptions::get_case_weights_file() const {
-  return case_weights_file;
+const std::vector<double>* SamplingOptions::get_case_weights() const {
+  return case_weights;
+}
+
+bool SamplingOptions::get_keep_in_bag() const {
+  return keep_in_bag;
 }
--- core/src/sampling/SamplingOptions.h
+++ core/src/sampling/SamplingOptions.h
@@ -3,21 +3,25 @@
 
 
 #include <string>
+#include <vector>
 
 class SamplingOptions {
 public:
   SamplingOptions(bool sample_with_replacement,
                   double sample_fraction,
-                  std::string case_weights_file);
+                  std::vector<double>* case_weights,
+                  bool keep_in_bag);
 
   bool get_sample_with_replacement() const;
   double get_sample_fraction() const;
-  const std::string& get_case_weights_file() const;
+  const std::vector<double>* get_case_weights() const;
+  bool SamplingOptions::get_keep_in_bag() const;
 
 private:
   bool sample_with_replacement;
   double sample_fraction;
-  std::string case_weights_file;
+  std::vector<double>* case_weights;
+  bool keep_in_bag;
 };
 
 #endif //GRADIENTFOREST_SAMPLINGOPTIONS_H
