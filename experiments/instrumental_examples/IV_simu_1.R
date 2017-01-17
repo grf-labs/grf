3e68d0fb73949f67c2da8c1adc2ad53a5ff93446
Stefan Wager <swager@stanford.edu>
2017-01-16 23:14:12 -0500

    Make causal forest function.

--- experiments/instrumental_examples/IV_simu_1.R
+++ experiments/instrumental_examples/IV_simu_1.R
@@ -23,7 +23,7 @@ A = rbinom(n, 1, 1/(1 + exp(X[,3])))
 W = Z * A
 Y = (W - 1/2) * tau(X[,1]) + 3/2 * (2*A - 1) * (X[,1] > 1/3) + 2 * rnorm(n)
 
-forest.causal = instrumental.forest(X, Y, W, W, min.node.size = 100)
+forest.causal = causal.forest(X, Y, W, min.node.size = 100)
 preds.causal = predict(forest.causal, X.test)
 
 forest.iv = instrumental.forest(X, Y, W, Z, min.node.size = 100)
---
+++ r-package/gradient.forest/R/causal.forest.R
@@ -0,0 +1,135 @@
+causal.forest <- function(X, Y, W,
+	sample.fraction=0.5,
+	mtry=ceiling(ncol(X)/3),
+	num.trees=500,
+	num.threads=NULL,
+	min.node.size=NULL,
+	keep.inbag = FALSE,
+	honesty=TRUE,
+	ci.group.size=2,
+	precompute.nuisance=TRUE,
+	seed=NULL) {
+
+	sparse.data <- as.matrix(0)
+
+	if (is.null(mtry)) {
+        mtry <- 0
+     } else if (!is.numeric(mtry) | mtry < 0) {
+        stop("Error: Invalid value for mtry")
+     }
+     
+     verbose <- FALSE
+    
+     if (is.null(num.threads)) {
+        num.threads <- 0
+     } else if (!is.numeric(num.threads) | num.threads < 0) {
+        stop("Error: Invalid value for num.threads")
+     }
+    
+     if (is.null(min.node.size)) {
+        min.node.size <- 0
+     } else if (!is.numeric(min.node.size) | min.node.size < 0) {
+        stop("Error: Invalid value for min.node.size")
+     }
+     
+     sample.with.replacement <- FALSE
+     
+     if (!is.logical(keep.inbag)) {
+        stop("Error: Invalid value for keep.inbag")
+     }
+     
+     if (!is.numeric(sample.fraction) | sample.fraction <= 0 | 
+        sample.fraction > 1) {
+        stop("Error: Invalid value for sample.fraction. Please give a value in (0,1].")
+     }
+
+     if (is.null(seed)) {
+        seed <- runif(1, 0, .Machine$integer.max)
+     }
+     
+     split.regularization <- 0
+     
+     if (!precompute.nuisance) {
+     	
+     	input.data <- as.matrix(cbind(X, Y, W))	
+     	
+     } else {
+     	
+     	forest.Y <- regression.forest(X, Y,
+			sample.fraction=sample.fraction,
+			mtry=mtry,
+			num.trees=min(500, num.trees),
+			num.threads=num.threads,
+			min.node.size=NULL,
+			keep.inbag = FALSE,
+			honesty=TRUE,
+			seed=seed)
+		Y.hat = predict(forest.Y)
+		
+		forest.W <- regression.forest(X, W,
+			sample.fraction=sample.fraction,
+			mtry=mtry,
+			num.trees=min(500, num.trees),
+			num.threads=num.threads,
+			min.node.size=NULL,
+			keep.inbag = FALSE,
+			honesty=TRUE,
+			seed=seed)
+		W.hat = predict(forest.W)
+		
+		input.data <- as.matrix(cbind(X, Y - Y.hat, W - W.hat))
+
+     }
+
+	
+	variable.names <- c(colnames(X), "outcome", "treatment")
+	outcome.index.zeroindexed <- ncol(X)
+	treatment.index.zeroindexed <- ncol(X) + 1
+	instrument.index.zeroindexed <- treatment.index.zeroindexed
+	
+	no.split.variables <- numeric(0)
+
+	forest <- instrumental_train(input.data,
+						outcome.index.zeroindexed,
+						treatment.index.zeroindexed,
+						instrument.index.zeroindexed,
+						sparse.data,
+						variable.names,
+						mtry,
+						num.trees,
+						verbose,
+						num.threads,
+						min.node.size,
+						sample.with.replacement,
+						keep.inbag,
+						sample.fraction,
+						no.split.variables,
+						seed,
+						honesty,
+						ci.group.size,
+						split.regularization)
+						
+						
+	class(forest) <- "causal.forest"
+	forest
+}
+
+predict.causal.forest <- function(forest, newdata, num.threads = NULL) {
+
+	if (is.null(num.threads)) {
+        num.threads <- 0
+     } else if (!is.numeric(num.threads) | num.threads < 0) {
+        stop("Error: Invalid value for num.threads")
+     }
+     
+	 sparse.data <- as.matrix(0)
+	 variable.names <- character(0)
+	
+	 input.data <- as.matrix(cbind(newdata, NA))	
+	
+     instrumental_predict(forest,
+     				  input.data,
+     				  sparse.data,
+     				  variable.names,
+     				  num.threads)    				 
+} 
