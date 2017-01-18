0bc526a0e3560a2b2e985f496a20e1060059dff8
Julie Tibshirani <julietibs@gmail.com>
2017-01-18 11:02:53 -0500

    Minor cleanup.

--- r-package/gradient.forest/tests/test_causal_variance_estimates.R
+++ r-package/gradient.forest/tests/test_causal_variance_estimates.R
@@ -3,8 +3,9 @@ library(gradient.forest)
 set.seed(1234)
 
 test_that("causal variance estimates are reasonable", {
-	p = 20
-	n = 2000
+
+	p = 2
+	n = 400
 
 	ticks = 101
 	X.test = matrix(0, ticks, p)
@@ -16,14 +17,15 @@ test_that("causal variance estimates are reasonable", {
 	W = rbinom(n, 1, 0.5)
 	Y = (X[,1] > 0) * (2 * W  - 1) + rnorm(n)
 
-	forest.causal = instrumental.forest(X, Y, W, W, sample.fraction = 0.1, min.node.size = 5, split.regularization = 0)
+	forest.causal = instrumental.forest(X, Y, W, W, sample.fraction = 0.5, num.trees = 10000, split.regularization = 0, ci.group.size = 4)
 	preds.causal = predict(forest.causal, X.test, estimate.variance=TRUE)
 
 	preds.med = preds.causal[[1]]
-	preds.low = preds.causal[[1]] - 1.96 * sqrt(preds.causal[[2]])
-	preds.high = preds.causal[[1]] + 1.96 * sqrt(preds.causal[[2]])
+	preds.low = preds.causal[[1]] - 1.96 * sqrt(pmax(0, preds.causal[[2]]))
+	preds.high = preds.causal[[1]] + 1.96 * sqrt(pmax(0, preds.causal[[2]]))
 
 	plot(X.test[,1], preds.med, ylim = range(preds.med, preds.low, preds.high))
     lines(X.test[,1], preds.low)
 	lines(X.test[,1], preds.high)
+
 })
