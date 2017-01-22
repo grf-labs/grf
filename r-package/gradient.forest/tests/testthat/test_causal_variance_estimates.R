10df1983fb3f95b28222575d1724b5c79ee42edb
Julie Tibshirani <julietibs@gmail.com>
2017-01-21 19:15:14 -0500

    Ensure the causal forest ETE test runs regularly.

--- r-package/gradient.forest/tests/testthat/test_causal_variance_estimates.R
+++ r-package/gradient.forest/tests/testthat/test_causal_variance_estimates.R
@@ -2,8 +2,7 @@ library(gradient.forest)
 
 set.seed(1234)
 
-test_that("causal variance estimates are reasonable", {
-
+test_that("causal variance estimates are positive", {
 	p = 2
 	n = 400
 
@@ -20,12 +19,5 @@ test_that("causal variance estimates are reasonable", {
 	forest.causal = instrumental.forest(X, Y, W, W, sample.fraction = 0.5, num.trees = 10000, split.regularization = 0, ci.group.size = 4)
 	preds.causal = predict(forest.causal, X.test, estimate.variance=TRUE)
 
-	preds.med = preds.causal[[1]]
-	preds.low = preds.causal[[1]] - 1.96 * sqrt(pmax(0, preds.causal[[2]]))
-	preds.high = preds.causal[[1]] + 1.96 * sqrt(pmax(0, preds.causal[[2]]))
-
-	plot(X.test[,1], preds.med, ylim = range(preds.med, preds.low, preds.high))
-    lines(X.test[,1], preds.low)
-	lines(X.test[,1], preds.high)
-
+	expect_true(all(preds.causal$variance.estimate > 0))
 })
