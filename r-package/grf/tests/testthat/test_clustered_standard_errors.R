3072997db116290c414167a4af76dc1098d51b1c
Luke Miner <lminer@users.noreply.github.com>
2018-03-09 10:41:20 -0800

    Adds basic test for clustered standard errors

---
+++ r-package/grf/tests/testthat/test_clustered_standard_errors.R
@@ -0,0 +1,41 @@
+library(grf)
+
+set.seed(4321)
+
+test_that("Clustered standard errors are greater than unclustered", {
+
+    cluster_size <- 20
+    # data sim
+    X <- rnorm(1000)
+    Y <- 5 + 2 * X + rnorm(1000)
+
+    X_cluster <- rep(X, cluster_size)
+    Y_cluster <- rep(Y, cluster_size)
+    clusters <- rep(1:1000, cluster_size)
+
+
+    forest_corrected <- regression_forest(matrix(X_cluster),
+                                          Y_cluster,
+                                          num.trees = 1000,
+                                          ci.group.size = 4,
+                                          clusters = clusters)
+    preds_corrected.oob <- predict(forest_corrected, estimate.variance = TRUE)
+
+    forest_no_cluster <- regression_forest(matrix(X),
+                                           Y,
+                                           num.trees = 1000,
+                                           ci.group.size = 4)
+    preds_no_cluster.oob <- predict(forest_no_cluster, estimate.variance = TRUE)
+
+    forest_uncorrected <- regression_forest(matrix(X_cluster),
+                                            Y_cluster,
+                                            num.trees = 1000,
+                                            ci.group.size = 4)
+    preds_uncorrected.oob <- predict(forest_uncorrected, estimate.variance = TRUE)
+
+    mean_no_cluster <- mean(preds_no_cluster.oob$variance.estimates)
+    mean_uncorrected <- mean(preds_uncorrected.oob$variance.estimates)
+    mean_corrected <- mean(preds_corrected.oob$variance.estimates)
+
+    expect_true(mean_uncorrected < mean_corrected)
+})
