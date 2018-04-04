c9acbb1a56162d71d24c68c0d35876d7afd54b2a
Julie Tibshirani <julietibs@gmail.com>
2018-04-04 15:08:56 -0700

    Clarify some internal naming in the splitting rules.

--- core/src/splitting/SplittingRule.h
+++ core/src/splitting/SplittingRule.h
@@ -26,7 +26,7 @@ public:
   virtual ~SplittingRule() {}
   virtual bool find_best_split(size_t node,
                                const std::vector<size_t>& possible_split_vars,
-                               const std::unordered_map<size_t, double>& responses_by_sample,
+                               const std::unordered_map<size_t, double>& labels_by_sample,
                                const std::vector<std::vector<size_t>>& samples,
                                std::vector<size_t>& split_vars,
                                std::vector<double>& split_values) = 0;
