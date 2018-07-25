f90610e377b4948038da12372521e27587af2a7e
Stefan Wager <swager@stanford.edu>
2018-07-24 22:08:13 -0700

    Address comments.

--- r-package/grf/R/forest_summary.R
+++ r-package/grf/R/forest_summary.R
@@ -1,17 +1,18 @@
-#' Omnibus test for presence of heterogeneity via calibration 
+#' Omnibus test for presence of heterogeneity via calibration.
 #' 
-#' Test calibration of the forest. A slope of 1 means the forest is
-#' well calibrated. The significance coefficient of the slope acts as
+#' Test calibration of the forest. Computes the best linear fit of the target
+#' estimand using the forest prediction (on held-out data) as the sole
+#' regressor. A slope of 1 means the forest is well calibrated.
+#' The significance coefficient of the slope acts as
 #' an omnibus test for the presence of heterogeneity: If the slope is
-#' not significantly different from 0, then we have not found evidence
-#' of heterogeneity.
+#' significantly different from 0, then we can reject the null of
+#' no heterogeneity.
 #'
 #' @param forest The trained forest.
-#'
-#' @return A heteroskedasticity-consistent test of calibration
-#'
+#' @return A heteroskedasticity-consistent test of calibration.
 #' @references Chernozhukov, Victor, Mert Demirer, Esther Duflo, and Ivan Fernandez-Val. "Generic Machine Learning Inference on Heterogenous Treatment Effects in Randomized Experiments." arXiv preprint arXiv:1712.04802 (2017).
-#' @examples \dontrun{
+#'
+#'  @examples \dontrun{
 #' n = 400; p = 5
 #' X = matrix(rnorm(n*p), n, p)
 #' W = rbinom(n, 1, 0.25 + 0.5 * (X[,1] > 0))
@@ -24,18 +25,20 @@
 test_calibration = function(forest) {
     if ("regression_forest" %in% class(forest)) {
         DF = data.frame(target = forest$Y.orig,
-                        pred = predict(forest)$predictions)
+                        forest.oob.pred = predict(forest)$predictions)
     } else if ("causal_forest" %in% class(forest)) {
         DF = data.frame(target = forest$Y.orig - forest$Y.hat,
-                        pred = (forest$W.orig - forest$W.hat) *
+                        forest.oob.pred = (forest$W.orig - forest$W.hat) *
                             predict(forest)$predictions)
-        
+    } else {
+        stop("Calibration check not supported for this type of forest.")
     }
-    
-    best.linear.predictor = lm(target ~ pred + 0, data = DF)
-    
-    # We need to use some extra packages to get
-    # heteroskedasticity-consistent inference.
-    lmtest::coeftest(best.linear.predictor,
-                     vcov = sandwich::vcovHC)
+    best.linear.predictor = lm(target ~ forest.oob.pred + 0, data = DF)
+    blp.summary = lmtest::coeftest(best.linear.predictor,
+                                   vcov = sandwich::vcovHC)
+    attr(blp.summary, "method") <-
+        paste("Slope of best linear fit using forest prediction (on held-out data)",
+              "as regressor, along with heteroskdasticity-robust (HC3) SEs",
+              sep="\n")
+    blp.summary
 }
--- r-package/grf/tests/testthat/test_forest_summaries.R
+++ r-package/grf/tests/testthat/test_forest_summaries.R
@@ -7,11 +7,12 @@ test_that("calibration is reasonable", {
     X = matrix(rnorm(n*p), n, p)
     W = rbinom(n, 1, 0.25 + 0.5 * (X[,1] > 0))
     Y = pmax(X[,1], 0) * (W - 0.75) + rnorm(n)
+
     cf = causal_forest(X, Y, W)
     test_calibration(cf)
     expect_true(abs(test_calibration(cf)[1] - 1) <= 0.4)
-    
-    regf = regression_forest(X, Y)
-    test_calibration(regf)
-    expect_true(abs(test_calibration(regf)[1]/test_calibration(regf)[2]) <= 3.5)
+
+    rf = regression_forest(X, Y)
+    test_calibration(rf)
+    expect_true(abs(test_calibration(rf)[1]/test_calibration(rf)[2]) <= 3.5)
 })
