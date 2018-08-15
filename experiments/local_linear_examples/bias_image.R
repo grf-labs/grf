e78f591311b9044bbc22f8b765a9ac665e10b098
Rina Siller Friedberg <rinafriedberg@gmail.com>
2018-08-15 13:45:56 -0700

    fixed bugs in bias image script file and added comments

--- experiments/local_linear_examples/bias_image.R
+++ experiments/local_linear_examples/bias_image.R
@@ -11,25 +11,28 @@ mu = function(x){ log(1 + exp(6 * x)) }
 
 # set parameters
 n = 600; p = 20; sigma = sqrt(20)
-X.test =  matrix(runif(n*p, -1, 1), nrow = n)
+X = matrix(runif(n*p, -1, 1), nrow = n)
+Y = mu(X[,1]) + sigma*rnorm(n)
 
-# show a grid of predictions along the first coordinate 
-ticks = seq(-1, 1, length=n) 
+# show a grid of predictions along the first coordinate
+X.test = matrix(runif(n*p, -1, 1), nrow = n)
+ticks = seq(-1, 1, length=n)
 X.test[,1] = ticks
 truth = mu(ticks)
 
-# train forest 
-forest = local_linear_forest(X, Y, tune.parameters = TRUE)
+# train forest
+ll.forest = local_linear_forest(X, Y, tune.parameters = TRUE)
+forest = regression_forest(X, Y, tune.parameters = TRUE)
 
-# lasso to select local linear correction variables 
+# lasso to select local linear correction variables
 lasso.mod = cv.glmnet(X, Y, alpha=1)
-coefs = coef(lasso.mod)
-selected = which(coefs != 0)
-# remove intercept and adjust indexes correspondingly 
-selected = selected[2:length(selected)] - 1 
+selected = which(coef(lasso.mod) != 0)
+
+# remove intercept and adjust indexes correspondingly
+selected = selected[2:length(selected)] - 1
 
 # make predictions
-preds.llf = predict(forest, X.test, linear.correction.variables = selected, tune.lambda = TRUE)$predictions
+preds.llf = predict(ll.forest, X.test, linear.correction.variables = selected, tune.lambda = TRUE)$predictions
 preds.rf = predict(forest, X.test)$predictions
 df = data.frame(cbind(ticks, truth, preds.llf, preds.rf))
 
