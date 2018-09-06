401b635cbebccc33fdbd6bfcca39d06d9f20b283
buyannemekh <munkh22b@mtholyoke.edu>
2018-09-06 12:14:30 -0700

    adding plot function to plot a tree in plot.R

---
+++ r-package/grf/R/plot.R
@@ -0,0 +1,71 @@
+create_dot_body <- function(nodes, index=1) {
+  
+  node <- nodes[[index]]
+  
+  # Leaf case: print label only
+  if (node$is_leaf) {
+    num_samples = length(node$samples)
+    line_label <- paste(index-1, ' [shape=box,style=filled,color=".7 .3 1.0" , label="leaf' , index,' \nsamples = ',num_samples,'"];')
+    return(line_label)
+  }
+  
+  # Non-leaf case: print label, child edges
+  if(!is.null(node$left_child)){
+    edge = paste(index-1, "->", node$left_child-1)
+    if(index ==1 ){
+      edge_info_left <- paste(edge, '[labeldistance=2.5, labelangle=45, headlabel="True"];')
+    }
+    else{
+      edge_info_left <- paste(edge, " ;")
+    }
+  } 
+  else{edge_info_right<-NULL}
+  
+  if(!is.null(node$right_child)){
+    edge = paste(index-1, "->", node$right_child-1)
+    if(index==1){
+      edge_info_right <- paste(edge, '[labeldistance=2.5, labelangle=-45, headlabel="False"]')
+    }
+    else{
+      edge_info_right <- paste(edge, " ;")
+    }
+  }else{edge_info_right<-NULL}
+  
+  node_info <- paste(index-1, '[label="split_variable', node$split_variable, '<=', round(node$split_value,2), '"] ;')
+  
+  this_lines <- paste(node_info,
+                      edge_info_left,
+                      edge_info_right, sep="\n")
+  
+  left_child_lines <- ifelse(!is.null(node$left_child),
+                             create_dot_body(nodes, index=node$left_child),
+                             NULL)
+  
+  right_child_lines <- ifelse(!is.null(node$right_child),
+                              create_dot_body(nodes, index=node$right_child),
+                              NULL)
+  
+  lines <- paste(this_lines, left_child_lines, right_child_lines, sep="\n")
+  
+  return(lines)
+}
+
+export_graphviz <- function(nodes){
+  header <- "digraph nodes { \n node [shape=box] ;"
+  footer <- "}"
+  body <- create_dot_body(nodes)
+  
+  dot_string <- paste(header, body, footer, sep="\n")
+  
+  return(dot_string)
+}
+
+#' Plot a GRF tree object.
+#' @param x The tree to plot
+#' @param ... Additional arguments (currently ignored).
+#' @export
+plot.grf_tree <- function(x){
+  dot_file <- export_graphviz(nodes=x$nodes)
+  DiagrammeR::grViz(dot_file)
+}
+
