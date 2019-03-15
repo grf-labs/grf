bb0015c3b6c86df7334c59da078fcd3f7e7ce088
Roland Stevenson <roland@rmg-services.com>
2019-03-15 14:50:40 +0000

    added tests for validating input #373

---
+++ r-package/grf/tests/testthat/test_validation.R
@@ -0,0 +1,67 @@
+library(grf)
+library(testthat)
+set.seed(1234)
+
+test_that("example data passes validation"){
+  n = 50; p = 10
+  X = matrix(rnorm(n*p), n, p)
+  W = rbinom(n, 1, 0.5)
+  Y = pmax(X[,1], 0) * W + X[,2] + pmin(X[,3], 0) + rnorm(n)
+  expect_error(validate_X(X), NA)
+  expect_error(validate_W(W), NA)
+  expect_error(validate_Y(Y), NA)
+}
+
+test_that("non-vector Y throws error", {
+  Y = matrix()
+  expect_error(validate_Y(Y))
+})
+
+test_that("non-vector W throws error", {
+  W = matrix()
+  expect_error(validate_W(W))
+})
+
+test_that("factor Y throws error", {
+  Y = c(1,2,3)
+  Y = as.factor(Y)
+  expect_error(validate_Y(Y))
+})
+
+test_that("character Y throws error", {
+  Y = c(1,2,3)
+  Y = as.character(Y)
+  expect_error(validate_Y(Y))
+})
+
+test_that("factor W throws error", {
+  W = c(0,1)
+  W = as.factor(W)
+  expect_error(validate_W(W))
+})
+
+test_that("character W throws error", {
+  W = c(0,1)
+  W = as.character(W)
+  expect_error(validate_W(W))
+})
+
+test_that("W must contain only two unique values", {
+  W = c(0,1,2)
+  expect_error(validate_W(W))
+})
+
+test_that("X with NA throws error", {
+  X = matrix(c(NA,2), nrow=1, ncol=2)
+  expect_error(validate_X(X))
+})
+
+test_that("Y with NA throws error", {
+  Y = c(NA,1,2)
+  expect_error(validate_Y(Y))
+})
+
+test_that("W with NA throws error", {
+  W = c(NA,1)
+  expect_error(validate_W(W))
+})
