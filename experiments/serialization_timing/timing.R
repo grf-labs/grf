50e28cf313489f76f9022cc34ae56855e618cd2c
David Abraham Hirshberg <davidahirshberg@gmail.com>
2019-04-01 12:08:31 -0700

    fixed serialization timing experiment

--- experiments/serialization_timing/timing.R
+++ experiments/serialization_timing/timing.R
@@ -17,30 +17,14 @@ X.new <- matrix(rnorm(n * p), n, p)
 system.time(replicate(50, regression_forest(X, Y, serialize=TRUE)))
 system.time(replicate(50, regression_forest(X, Y, serialize=FALSE)))
 
-## save and load forest so that it is serialized w/no pointer 
-forest.a = regression_forest(X, Y, serialize=TRUE)
-forest.b = save_and_load(forest.a)
+## save and load forest so forest.b is serialized w/no xptr
+forest.a = lapply(1:50, function(i) { regression_forest(X, Y, serialize=TRUE) })
+forest.b = lapply(forest.a, save_and_load)
 
 ## Time predictions with and without deserialization
-# because forest is deserialized on the first call,
-# this should be slow, fast, equally fast
-system.time(predict(forest, X.new))
-system.time(predict(forest, X.new))
-system.time(predict(nonserialized.forest, X.new))
+# because forest.b is deserialized on the first call,
+# this should be fast, slow, fast
+system.time(lapply(forest.a, function(forest) { predict(forest, X.new) }))
+system.time(lapply(forest.b,  function(forest) { predict(forest, X.new) }))
+system.time(lapply(forest.b,  function(forest) { predict(forest, X.new) }))
 
-
-p = 10
-n = 500
-i = 5
-X = matrix(2 * runif(n * p) - 1, n, p)
-Y = rnorm(n)  + 100 * (X[,i] > 0)
-X.new = matrix(2 * runif(n * p) - 1, n, p)
-
-forest.a = quantile_forest(X, Y)
-forest.b = save_and_load(forest.a)
-forest.c = save_and_load(forest.a)
-predict(forest.a, X[1:4,])
-predict(forest.b, X[1:4,])
-predict(forest.c, X[1:4,])
-
-all(predict(forest.b,X.new) == predict(forest.c,X.new))
