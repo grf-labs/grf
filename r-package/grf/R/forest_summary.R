c41caea13c3f1a59a18a303b4f7eadef6087d2ad
Roland Stevenson <roland@rmg-services.com>
2019-04-29 23:58:39 +0000

    condense one-sided pval calc; code doc #410

--- r-package/grf/R/forest_summary.R
+++ r-package/grf/R/forest_summary.R
@@ -68,11 +68,11 @@ test_calibration = function(forest) {
   attr(blp.summary, "method") <-
     paste("Best linear fit using forest predictions (on held-out data)",
           "as well as the mean forest prediction as regressors, along",
-          "with heteroskedasticity-robust (HC3) SEs",
+          "with one-sided heteroskedasticity-robust (HC3) SEs",
           sep="\n")
+  #convert to one-sided p-values
   dimnames(blp.summary)[[2]][4] <- gsub("[|]", "", dimnames(blp.summary)[[2]][4])
-  blp.summary[,4] <- blp.summary[,4]/2
-  blp.summary[,4] <- ifelse(sign(blp.summary[,3])<0, 1-blp.summary[,4],blp.summary[,4])
+  blp.summary[,4] <- ifelse(blp.summary[,3]<0, 1-blp.summary[,4]/2,blp.summary[,4]/2)
   blp.summary
 
 }
