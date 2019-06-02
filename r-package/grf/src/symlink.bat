2753d53eac7d3381e7fcede761d552e7d5562508
Erik Sverdrup <erik.sverdrup@gmail.com>
2019-06-02 16:59:20 -0700

    add symlink

---
+++ r-package/grf/src/symlink.bat
@@ -0,0 +1,1 @@
+# https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/mklink
---
+++ r-package/grf/src/symlink.sh
@@ -0,0 +1,4 @@
+# Creates a symlink to grf's C++ dependencies
+ln -s ../../../core/src/
+ln -s ../../../core/third_party/optional/
+ln -s ../bindings/* .
