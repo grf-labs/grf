b4fb1872e983450f62a34272c802a39f059d8ee9
Julie Tibshirani <jtibs@cs.stanford.edu>
2019-06-01 20:00:10 -0700

    Remove a boosting test based on incorrect assumptions.

--- r-package/grf/tests/testthat/test_boosted_regression_forest.R
+++ r-package/grf/tests/testthat/test_boosted_regression_forest.R
@@ -3,7 +3,6 @@ library(grf)
 set.seed(1)
 
 test_that("Boosted regression forest improves performance vs regular forest", {
-
   n <- 500; p <- 6
   X <- matrix(runif(n * p), n, p)
   mu <- 2 * X[,1] * X[,2] + 3 * X[,3] + 4 * X[,4]
@@ -19,27 +18,13 @@ test_that("Boosted regression forest improves performance vs regular forest", {
   expect_true(mse.boost < mse.forest)
 })
 
-
 test_that("Boosted forest takes user specified number of steps",  {
   n <- 100; p <- 6
   X <- matrix(runif(n * p), n, p)
   mu <- 2 * X[,1] * X[,2] + 3 * X[,3] + 4 * X[,4]
   Y <- mu + rnorm(n)
   forest.boost <- boosted_regression_forest(X,Y,boost.steps=2)
-  expect_equal(2,length(forest.boost$forests))
-})
-
-test_that("Under cross-validation, errors decrease each step", {
-  n<-200; p<-6
-  X <- matrix(runif(n * p), n, p)
-  mu <- 2 * X[,1]^2 * X[,2] + 3 * X[,3] + 4 * X[,4]
-  Y <- mu + rnorm(n)
-  forest.boost <- boosted_regression_forest(X,Y)
-  errors <- unlist(forest.boost$error)
-  if(length(errors)>1) {
-    improves <- errors[2:length(errors)] - errors[1:(length(errors)-1)]
-    expect_true(all(improves<0))
-  }
+  expect_equal(2, length(forest.boost$forests))
 })
 
 test_that("boost.error.reduction validation works", {
