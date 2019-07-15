eea8f0c95ab2cdc6751cb17c779376c6e729865f
halflearned <halflearned@gmail.com>
2019-07-15 15:19:01 -0700

    deleting most of boost

--- core/third_party/boost/version.hpp
+++ core/third_party/boost/version.hpp
@@ -1,32 +1,19 @@
-//  Boost version.hpp configuration header file  ------------------------------//
+/*=============================================================================
+    Copyright (c) 2001-2003 Hartmut Kaiser
+    http://spirit.sourceforge.net/
 
-//  (C) Copyright John maddock 1999. Distributed under the Boost
-//  Software License, Version 1.0. (See accompanying file
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+#if !defined(SPIRIT_CLASSIC_VERSION_HPP)
+#define SPIRIT_CLASSIC_VERSION_HPP
 
-//  See http://www.boost.org/libs/config for documentation
-
-#ifndef BOOST_VERSION_HPP
-#define BOOST_VERSION_HPP
-
+///////////////////////////////////////////////////////////////////////////////
 //
-//  Caution: this is the only Boost header that is guaranteed
-//  to change with every Boost release. Including this header
-//  will cause a recompile every time a new Boost version is
-//  used.
+//  This is the version of the current Spirit distribution
 //
-//  BOOST_VERSION % 100 is the patch level
-//  BOOST_VERSION / 100 % 1000 is the minor version
-//  BOOST_VERSION / 100000 is the major version
+///////////////////////////////////////////////////////////////////////////////
+#define SPIRIT_VERSION 0x1811
+#define SPIRIT_PIZZA_VERSION SPIRIT_MEGA_VEGGI  // :-)
 
-#define BOOST_VERSION 107000
-
-//
-//  BOOST_LIB_VERSION must be defined to be the same as BOOST_VERSION
-//  but as a *string* in the form "x_y[_z]" where x is the major version
-//  number, y is the minor version number, and z is the patch level if not 0.
-//  This is used by <config/auto_link.hpp> to select which library version to link to.
-
-#define BOOST_LIB_VERSION "1_70"
-
-#endif
+#endif // defined(SPIRIT_VERSION_HPP)
