8ca063b515a3484e4a89a6966b2b2341844b023f
Jinhua Wang <jinhuawang@Jinhuas-MacBook-Pro.local>
2019-08-25 17:20:53 +0100

    fixed the comment format to save the tree graphs

--- r-package/grf/R/plot.R
+++ r-package/grf/R/plot.R
@@ -84,15 +84,24 @@ export_graphviz <- function(tree) {
 }
 
 #' Plot a GRF tree object.
-#' 
 #' @param x The tree to plot
 #' @param ... Additional arguments (currently ignored).
 #'
 #' @method plot grf_tree
-#' @examples 
-#' If you would like to save the plots, you can do so by installing the DiagrammeRsvg package and running the command: 
-#' tree.plot = plot(tree)
+#' @examples
+#'{ # Save the plot of a tree in the causal forest.
+#' install.packages("DiagrammeR")
+#' install.packages("DiagrammeRsvg")
+#' n <- 500
+#' p <- 10
+#' X <- matrix(rnorm(n * p), n, p)
+#' W <- rbinom(n, 1, 0.5)
+#' Y <- pmax(X[, 1], 0) * W + X[, 2] + pmin(X[, 3], 0) + rnorm(n)
+#' c.forest <- causal_forest(X, Y, W)
+#' #save the first tree in the forest as plot.svg
+#' tree.plot = plot(get_tree(c.forest, 1))
 #' cat(DiagrammeRsvg::export_svg(tree.plot), file='plot.svg')
+#'}
 #' @export
 plot.grf_tree <- function(x, ...) {
   if (!requireNamespace("DiagrammeR", quietly = TRUE)) {
