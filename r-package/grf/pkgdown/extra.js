41631b9c933997067068082ab91fc964980346a0
Erik Sverdrup <erik.sverdrup@gmail.com>
2019-11-21 18:34:10 -0800

    Add pkgdown workaround for relative links

---
+++ r-package/grf/pkgdown/extra.js
@@ -0,0 +1,7 @@
+// From https://github.com/microsoft/LightGBM/blob/master/R-package/pkgdown/extra.js
+$(function() {
+    if(window.location.pathname.toLocaleLowerCase().indexOf('/r/reference') != -1) {
+        /* Replace '/R/' with '/r-package/grf/R/' in all external links to .R files of grf GitHub repo */
+        $('a[href^="https://github.com/grf-labs/grf/blob/master/R"][href*=".R"]').attr('href', (i, val) => { return val.replace('/R/', '/r-package/grf/R/'); });
+    }
+});
