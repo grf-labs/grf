2d3c55324c5f7fcf3776786fae4557eaeb3a4ffd
Erik Sverdrup <erik.sverdrup@gmail.com>
2019-11-23 15:16:40 -0800

    Add pkgdown workaround for reference urls (#575)

---
+++ r-package/grf/pkgdown/extra.js
@@ -0,0 +1,10 @@
+// This is a workaround to get the online documentation to produce the correct GitHub
+// link to the source file in question. pkgdown assumes the package is hosted
+// under the root repo, but grf is hosted under /r-package/grf.
+// This fix is from: https://github.com/r-lib/pkgdown/issues/1152
+$(function() {
+    if(window.location.pathname.toLocaleLowerCase().indexOf('/reference') != -1) {
+        /* Replace '/R/' with '/r-package/grf/R/' in all external links to .R files of grf GitHub repo */
+        $('a[href^="https://github.com/grf-labs/grf/blob/master/R"][href*=".R"]').attr('href', (i, val) => { return val.replace('/R/', '/r-package/grf/R/'); });
+    }
+});
