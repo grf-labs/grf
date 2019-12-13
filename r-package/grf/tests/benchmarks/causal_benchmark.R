148eb6487ea906783d26552dd6c6ae72f9542d6f
Erik Sverdrup <erik.sverdrup@gmail.com>
2019-12-13 15:26:11 -0800

    Update tests/benchmarks/ (#596)
    Update tests/benchmarks/causal_benchmark.R and delete an old unused
    file.

--- r-package/grf/tests/benchmarks/causal_benchmark.R
+++ r-package/grf/tests/benchmarks/causal_benchmark.R
@@ -1,134 +1,75 @@
-rm(list = ls())
+# grf benchmark script
+# Usage: `(benchmarks)$ Rscript causal_benchmark.R`
+
 library(grf)
+source("../../../../experiments/benchmarking/dgps.R")
+set.seed(1)
 
-generate_data <- function(setup) {
-  if (setup == 1) {
-    n <- 600
-    p <- 8
-    X <- matrix(runif(n * p), n, p)
-    propensity <- (1 + dbeta(X[, 3], 2, 4)) / 4
-    tau <- (1 + 1 / (1 + exp(-20 * (X[, 1] - 0.3)))) * (1 + 1 / (1 + exp(-20 * (X[, 2] - 0.3))))
-    W <- rbinom(n, 1, 0.9)
-    Y <- 2 * X[, 3] - 1 + (W - 0.5) * tau + rnorm(n)
-  } else if (setup == 2) {
-    n <- 1600
-    p <- 6
-    k <- 3
-    X <- matrix(rnorm(n * p), n, p)
-    a <- rowMeans(X[, 1:k]) * sqrt(k)
-    x <- sign(a) * a^2
-    tau <- 1 / (1 + exp(-X[, p]))
-    propensity <- pmax(0.05, pmin(1 / (1 + exp(-x)), 0.95))
-    mu <- x - tau * (propensity - 0.5)
-    W <- rbinom(n, 1, propensity)
-    Y <- mu + W * tau + rnorm(n)
-  } else if (setup == 3) {
-    n <- 1000
-    p <- 10
-    X <- matrix(runif(n * p), n, p)
-    tau <- (1 + 1 / (1 + exp(-20 * (X[, 1] - 0.3)))) * (1 + 1 / (1 + exp(-20 * (X[, 2] - 0.3))))
-    W <- rbinom(n, 1, 0.9)
-    Y <- (W - 0.9) * tau + rnorm(n)
-  } else if (setup == 4) {
-    n <- 1600
-    p <- 20
-    X <- matrix(runif(n * p), n, p)
-    propensity <- (1 + dbeta(X[, 3], 2, 4)) / 4
-    tau <- rep(0, n)
-    W <- rbinom(n, 1, 0.9)
-    Y <- 2 * X[, 3] - 1 + (W - 0.5) * tau + rnorm(n)
-  } else if (setup == 5) {
-    n <- 4000
-    p <- 10
-    X <- matrix(rnorm(n * p), n, p)
-    W <- rbinom(n, 1, 0.1)
-    tau <- 0.2 * (X[, 3] > 0)
-    Y <- X[, 1] + X[, 2] + tau * W + rnorm(n)
-  }
-  list(X = X, W = W, Y = Y, tau = tau, n = n, p = p)
-}
+reps <- 10
+num.trees <- 2000
 
-evaluate_method <- function(estimate_tau, setup = 1) {
-  data <- generate_data(setup)
-  tau.hat <- estimate_tau(data$X, data$Y, data$W)
-  plot(data$tau, tau.hat)
-  abline(0, 1)
-  sqrt(mean((tau.hat - data$tau)^2))
-}
+results.raw <- lapply(1:reps, function(iter) {
+  data <- gen_data(4000, 10, dgp = "simple")
+  data.test <- gen_data(4000, 10, dgp = "simple")
 
-make_causal_forest <- function(stabilize.splits, min.node.size,
-                               alpha, imbalance.penalty) {
-  function(X, Y, W) {
-    cf <- causal_forest(X, Y, W,
-      stabilize.splits = stabilize.splits, alpha = alpha,
-      min.node.size = min.node.size, imbalance.penalty = imbalance.penalty
-    )
-    cf.pred <- predict(cf)
-    print(cf)
-    cf.pred$predictions
-  }
-}
+  Y.time <- system.time(forest.Y <- regression_forest(data$X, data$Y,
+                                                     num.trees = num.trees, num.threads=1))
+  Y.time.pred <- system.time(Y.test <- predict(forest.Y, newdata = data.test$X, num.threads=1))
+  Y.time.ci <- system.time(Y.test.ci <- predict(forest.Y, data.test$X,
+                                               estimate.variance = TRUE, num.threads=1))
+  Y.hat <- predict(forest.Y)$predictions
 
-res.untuned.unstab <- sapply(1:5, function(setup) {
-  evaluate_method(function(X, Y, W) {
-    cf <- causal_forest(X, Y, W, tune.parameters = "none", stabilize.splits = FALSE)
-    cf.pred <- predict(cf)
-    cf.pred$predictions
-  }, setup)
+  Y.error <- c(
+    oob = mean((Y.hat - data$m)^2),
+    test = mean((Y.test$predictions - data.test$m)^2),
+    stdratio = mean((Y.test.ci$predictions - data.test$m)^2 /
+                      Y.test.ci$variance.estimates)
+  )
+
+  tau.time <- system.time(forest.tau <- causal_forest(data$X, data$Y, data$W,
+                                                     Y.hat=Y.hat, W.hat = data$e,
+                                                     num.trees = num.trees, num.threads=1))
+  tau.time.pred <- system.time(tau.test <- predict(forest.tau, data.test$X, num.threads=1))
+  tau.time.ci <- system.time(tau.test.ci <- predict(forest.tau, data.test$X,
+                                                   estimate.variance = TRUE, num.threads=1))
+
+  tau.error <- c(
+    oob = mean((predict(forest.tau)$predictions - data$tau)^2),
+    test = mean((tau.test$predictions - data.test$tau)^2),
+    stdratio = mean((tau.test.ci$predictions - data.test$tau)^2 /
+                      tau.test.ci$variance.estimates)
+  )
+
+  error <- rbind(Y.error, tau.error)
+  time <- rbind(Y.time, Y.time.pred, Y.time.ci,
+                tau.time, tau.time.pred, tau.time.ci)
+
+  print("done with a rep!")
+  list(error, time[,1:3])
 })
-res.untuned.unstab
 
-res.tuned.unstab <- sapply(1:5, function(setup) {
-  evaluate_method(function(X, Y, W) {
-    cf <- causal_forest(X, Y, W, tune.parameters = "all", stabilize.splits = FALSE)
-    cf.pred <- predict(cf)
-    cf.pred$predictions
-  }, setup)
-})
-res.tuned.unstab
+avg.error = Reduce(function(a, b) a + b, lapply(results.raw, function(rr) rr[[1]])) / reps
+avg.time = Reduce(function(a, b) a + b, lapply(results.raw, function(rr) rr[[2]])) / reps
 
-res.untuned.stab <- sapply(1:5, function(setup) {
-  evaluate_method(function(X, Y, W) {
-    cf <- causal_forest(X, Y, W, min.node.size = 5, tune.parameters = "none", stabilize.splits = TRUE)
-    cf.pred <- predict(cf)
-    cf.pred$predictions
-  }, setup)
-})
-res.untuned.stab
+se.error = sqrt((Reduce(function(a, b) a + b,
+                        lapply(results.raw, function(rr) (rr[[1]] - avg.error)^2)) /
+                   (reps * (reps - 1))))
+se.time = sqrt((Reduce(function(a, b) a + b,
+                       lapply(results.raw, function(rr) (rr[[2]] - avg.time)^2)) /
+                  (reps * (reps - 1))))
 
-res.tuned.stab <- sapply(1:5, function(setup) {
-  evaluate_method(function(X, Y, W) {
-    cf <- causal_forest(X, Y, W, tune.parameters = "all", stabilize.splits = TRUE)
-    cf.pred <- predict(cf)
-    cf.pred$predictions
-  }, setup)
-})
-res.tuned.stab
+err.print = outer(1:nrow(avg.error), 1:ncol(avg.error), Vectorize(function(ii, jj) {
+  paste(sprintf("%.3f", avg.error[ii, jj]), "+/-", sprintf("%.3f", se.error[ii, jj]))
+}))
 
-res.untuned.unstab
-res.tuned.unstab
-res.untuned.stab
-res.tuned.stab
+time.print = outer(1:nrow(avg.time), 1:ncol(avg.time), Vectorize(function(ii, jj) {
+  paste(sprintf("%.3f", avg.time[ii, jj]), "+/-", sprintf("%.3f", se.time[ii, jj]))
+}))
 
+colnames(err.print) = colnames(avg.error)
+rownames(err.print) = rownames(avg.error)
+colnames(time.print) = colnames(avg.time)
+rownames(time.print) = rownames(avg.time)
 
-# res.ip = outer(c(0, 0.25, 0.5, 2, 4), 1:5,
-#             FUN = Vectorize(function(imbalance.penalty, setup) {
-#               evaluate_method(make_causal_forest(
-#                 stabilize.splits = TRUE,
-#                 min.node.size = 5,
-#                 alpha = 0.05,
-#                 imbalance.penalty = imbalance.penalty),
-#                 setup)
-#             }))
-#
-# res.mns = outer(c(0, 1, 2, 4, 8, 16, 32), 1:5,
-#                 FUN = Vectorize(function(mns, setup) {
-#                   evaluate_method(make_causal_forest(
-#                     stabilize.splits = TRUE,
-#                     min.node.size = mns,
-#                     alpha = 0.05,
-#                     imbalance.penalty = 0),
-#                     setup)
-#                 }))
-# colnames(res.mns) = sapply(1:5, function(ii) paste("setup", ii))
-# rownames(res.mns) = sapply(c(0, 1, 2, 4, 8, 16, 32), function(ii) paste("min. node size", ii))
+err.print
+time.print
