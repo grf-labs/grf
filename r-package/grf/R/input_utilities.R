957378d52cd2685498290f3b34cc45a47d308cc2
Rina Siller Friedberg <rinafriedberg@gmail.com>
2020-01-29 13:10:15 -0800

    updated style in input utilities

--- r-package/grf/R/input_utilities.R
+++ r-package/grf/R/input_utilities.R
@@ -142,9 +142,9 @@ validate_ll_path <- function(lambda.path) {
 validate_ll_cutoff <- function(ll.split.cutoff,  num.rows) {
    if (is.null(ll.split.cutoff)) {
      ll.split.cutoff <- floor(sqrt(num.rows))
-   } else if (!is.numeric(ll.split.cutoff) | length(ll.split.cutoff) > 1) {
+   } else if (!is.numeric(ll.split.cutoff) || length(ll.split.cutoff) > 1) {
      stop("LL split cutoff must be NULL or a scalar")
-   } else if (ll.split.cutoff < 0 | ll.split.cutoff > num.rows) {
+   } else if (ll.split.cutoff < 0 || ll.split.cutoff > num.rows) {
      stop("Invalid range for LL split cutoff")
    }
    ll.split.cutoff
