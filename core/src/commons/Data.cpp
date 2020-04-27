dbb9edde18f6524658daac4ec5ffc9e9f21c927d
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-04-26 21:02:23 -0700

    Fix accidental indentation

--- core/src/commons/Data.cpp
+++ core/src/commons/Data.cpp
@@ -226,11 +226,11 @@ double Data::get_weight(size_t row) const {
 }
 
 bool Data::get_censor(size_t row) const {
-    if (get(row, censor_index.value()) == 1.0) {
-      return true;
-    } else {
-      return false;
-    }
+  if (get(row, censor_index.value()) == 1.0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const std::set<size_t>& Data::get_disallowed_split_variables() const {
