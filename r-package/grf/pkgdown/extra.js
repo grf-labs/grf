7c9d33c2babf87d4492f47ec7aa449a7b21a429c
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-05-01 15:00:19 -0700

    Update extra.js

--- r-package/grf/pkgdown/extra.js
+++ r-package/grf/pkgdown/extra.js
@@ -7,4 +7,8 @@ $(function() {
         /* Replace '/R/' with '/r-package/grf/R/' in all external links to .R files of grf GitHub repo */
         $('a[href^="https://github.com/grf-labs/grf/blob/master/R"][href*=".R"]').attr('href', (i, val) => { return val.replace('/R/', '/r-package/grf/R/'); });
     }
+    if(window.location.pathname.toLocaleLowerCase().indexOf('/articles') != -1) {
+        /* Replace '/vignettes/' with '/r-package/grf/vignettes/' in all external links to .Rmd files of grf GitHub repo */
+        $('a[href^="https://github.com/grf-labs/grf/blob/master/vignettes"][href*=".Rmd"]').attr('href', (i, val) => { return val.replace('/vignettes/', '/r-package/grf/vignettes/'); });
+    }
 });
