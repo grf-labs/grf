49ba3fc5218d6d07e82e881a7a0ccea3872d8c9c
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-07-08 17:33:19 -0700

    Add original R file for Figure 3 (Angrist-Evans)

--- experiments/aos19/README.md
+++ experiments/aos19/README.md
@@ -1,7 +1,7 @@
 _This folder has replication files for the paper "Generalized Random Forests" by Athey, Tibshirani, and Wager._
 
 - For Figure 2, see the folder `quantile_examples`
-- For Figure 3, see X
+- For Figure 3, see the file `grow_forest_angrist_evans.R`
 - For Figure 5, see the folder `instrumental_examples`
 - For Table 1, these are simulations using a `causal_forest` with the DGPs _aw1_ and _aw2_ from `benchmarking/dgps.R` (parent folder)
 - For Table 2, see the folder `instrumental_stress_test`
---
+++ experiments/aos19/grow_forest_angrist_evans.R
@@ -0,0 +1,55 @@
+rm(list = ls())
+library(grf)
+load("R_files/angev80_recode_run1_line525.RData")
+
+FEATURES=data.frame(twoa.agem,
+			  	twoa.agefstm,
+			  	twoa.educm,
+			  	as.numeric(twoa.blackm),
+			  	as.numeric(twoa.hispm),
+			  	as.numeric(twoa.othracem),
+			  	twoa.incomed=round(twoa.incomed))
+names(FEATURES)=1:ncol(FEATURES)
+
+#labor income: twoa.incomem, worked for pay: twoa.workedm
+DF.all=data.frame(
+			  X=FEATURES,
+			  Y=as.numeric(twoa.workedm),
+			  W=as.numeric(twoa.kidcount > 2),
+			  I=as.numeric(twoa.samesex))
+
+# remove ~15% of data with missing father's income
+# roughly 4% of fathers have zero income after removing missing
+#
+# only consider married women
+is.ok = !is.na(twoa.incomed) & (twoa.marital==0)
+DF=DF.all[is.ok,]
+
+agefstm.vals= c(18, 20, 22, 24)
+incomed.vals = quantile(twoa.incomed, na.rm=TRUE, seq(0.025, 0.975, by = 0.05))
+
+dummy = expand.grid(AGEFSTM=agefstm.vals, INCOMED=incomed.vals)
+X.test = data.frame(
+	median(twoa.agem, na.rm=TRUE),
+	dummy[,1],
+	12,
+	0, 0, 1,
+	dummy[,2])
+names(X.test)=1:ncol(X.test)
+
+DF$Y.resid = DF$Y - predict(lm(Y ~ ., data = DF[,1:8]))
+
+W.lr = glm(W ~ ., data = DF[,c(1:7, 9)], family = binomial())
+DF$W.resid = DF$W - predict(W.lr, type="response")
+
+print(Sys.time())
+
+forest.iv = instrumental.forest(DF[,1:ncol(FEATURES)], DF$Y.resid, DF$W.resid, DF$I, min.node.size = 800, num.trees = 100000, ci.group.size = 125, sample.fraction = 0.05, precompute.nuisance = FALSE)
+preds.iv = predict(forest.iv, X.test, estimate.variance = TRUE)
+tau.hat = preds.iv$predictions
+var.hat = preds.iv$variance.estimates
+
+output = data.frame(dummy, TAU=tau.hat, VAR=var.hat)
+write.table(output, file="familysize.out")
+
+print(Sys.time())
