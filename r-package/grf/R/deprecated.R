ebb19fd5ce67ce2c123faa6fefd42e6c292b532e
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-08-31 10:25:02 -0700

    Add version number to deprecated message

--- r-package/grf/R/deprecated.R
+++ r-package/grf/R/deprecated.R
@@ -9,7 +9,7 @@
 #'
 #' @export
 average_late <- function(forest, ...) {
-  stop("This function has been deprecated. See the function `average_treatment_effect` instead.")
+  stop("This function has been deprecated after version 1.2.0. See the function `average_treatment_effect` instead.")
 }
 
 #' Average partial effect (deprecated)
@@ -23,5 +23,5 @@ average_late <- function(forest, ...) {
 #'
 #' @export
 average_partial_effect <- function(forest, ...) {
-  stop("This function has been deprecated. See the function `average_treatment_effect` instead.")
+  stop("This function has been deprecated after version 1.2.0. See the function `average_treatment_effect` instead.")
 }
