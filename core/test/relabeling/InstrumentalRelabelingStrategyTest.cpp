a1e66cd090d494293d644a4f361122b71f12f4ed
Erik Sverdrup <erik.sverdrup@gmail.com>
2020-11-05 16:22:13 -0800

    Add sample weights to instrumental relabeling (#752)

--- core/test/relabeling/InstrumentalRelabelingStrategyTest.cpp
+++ core/test/relabeling/InstrumentalRelabelingStrategyTest.cpp
@@ -27,11 +27,15 @@
 
 using namespace grf;
 
-std::vector<double> get_relabeled_outcomes(std::vector<double> observations, size_t num_samples) {
+std::vector<double> get_relabeled_outcomes(
+  std::vector<double> observations, size_t num_samples, bool use_sample_weights=false) {
   DefaultData data(observations, num_samples, 3);
   data.set_outcome_index(0);
   data.set_treatment_index(1);
   data.set_instrument_index(2);
+  if (use_sample_weights) {
+    data.set_weight_index(3);
+  }
 
   std::vector<size_t> samples;
   for (size_t i = 0; i < num_samples; ++i) {
@@ -123,4 +127,46 @@ TEST_CASE("constant instrument leads to no splitting", "[instrumental, relabelin
 
   std::vector<double> relabeled_outcomes = get_relabeled_outcomes(observations, 10);
   REQUIRE(relabeled_outcomes.empty()); // An empty map signals that no splitting should be performed.
+}
+
+TEST_CASE("mean influence is zero", "[instrumental, relabeling]") {
+  std::vector<double> observations = {
+      -9.99984, -7.36924, 5.11211, -0.826997, 0.655345, -5.62082, -9.05911, 3.57729, 3.58593, 8.69386, // outcomes
+      1, 0, 1, 0, 1, 0, 1, 0, 1, 1,  // treatment
+      1, 0, 1, 0, 1, 0, 1, 0, 1, 1,  // instrument = treatment
+      1.08, 1.94, 1.5, 0.2, 0.62, 2.5, 1.84, 1.16, 0.78, 0.12 // sample weights
+  };
+
+  // Without sample weights
+  std::vector<double> relabeled_outcomes = get_relabeled_outcomes(observations, 10, false);
+  double average = 0;
+  for (size_t sample = 0; sample < 10; sample++) {
+    average += relabeled_outcomes[sample];
+  }
+  average /= 10;
+  REQUIRE(equal_doubles(average, 0, 1e-10));
+
+  // With sample weights
+  std::vector<double> relabeled_outcomes_weighted = get_relabeled_outcomes(observations, 10, true);
+  double average_weighted = 0;
+  double weight_sum = 0;
+  for (size_t sample = 0; sample < 10; sample++) {
+    double weight = observations[30 + sample];
+    average_weighted += relabeled_outcomes_weighted[sample] * weight;
+    weight_sum += weight;
+  }
+  average_weighted /= weight_sum;
+  REQUIRE(equal_doubles(average_weighted, 0, 1e-10));
+
+  // Instrumental with sample weights
+  std::vector<double> instrument = {0, 1, 1, 0, 0, 1, 0, 0, 1, 1};
+  std::copy(instrument.begin(), instrument.end(), observations.begin() + 20);
+  std::vector<double> relabeled_outcomes_weighted_iv = get_relabeled_outcomes(observations, 10, true);
+  double average_weighted_iv = 0;
+  for (size_t sample = 0; sample < 10; sample++) {
+    double weight = observations[30 + sample];
+    average_weighted_iv += relabeled_outcomes_weighted_iv[sample] * weight;
+  }
+  average_weighted_iv /= weight_sum;
+  REQUIRE(equal_doubles(average_weighted_iv, 0, 1e-10));
 }
