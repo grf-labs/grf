7e86842297c6a39b4554d0878f581116fe6db168
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-01-06 21:30:21 -0800

    Update instrumental forest tuning

--- r-package/grf/R/deprecated.R
+++ r-package/grf/R/deprecated.R
@@ -58,3 +58,23 @@ tune_regression_forest <- function(X, Y, ...) {
 tune_causal_forest <- function(X, Y, W, Y.hat, W.hat, ...) {
   stop("This function has been deprecated after version 1.2.0.")
 }
+
+#' Instrumental forest tuning (deprecated)
+#'
+#' To tune a instrumental forest, see the function `instrumental_forest`
+#'
+#' @param X X
+#' @param Y Y
+#' @param W W
+#' @param Z Z
+#' @param Y.hat Y.hat
+#' @param W.hat W.hat
+#' @param Z.hat Z.hat
+#' @param ... Additional arguments (currently ignored).
+#'
+#' @return output
+#'
+#' @export
+tune_instrumental_forest <- function(X, Y, W, Z, Y.hat, W.hat, Z.hat, ...) {
+  stop("This function has been deprecated after version 1.2.0.")
+}
