3c5792843789e7aa4cf14d4970a35e4176c7435b
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-02-02 11:21:42 -0800

    no comment needed

--- core/src/commons/Data.h
+++ core/src/commons/Data.h
@@ -103,10 +103,6 @@ public:
 
   double get_causal_survival_denominator(size_t row) const;
 
-  /**
-   *
-   *
-   */
   bool is_failure(size_t row) const;
 
   const std::set<size_t>& get_disallowed_split_variables() const;
