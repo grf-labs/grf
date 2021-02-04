31b994c0d8b61fe7f3d207b6bbd3707686a88038
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-02-03 17:42:20 -0800

    Same reasoning for empty variance

--- core/src/prediction/collector/OptimizedPredictionCollector.cpp
+++ core/src/prediction/collector/OptimizedPredictionCollector.cpp
@@ -120,7 +120,7 @@ std::vector<Prediction> OptimizedPredictionCollector::collect_predictions_batch(
     if (num_leaves == 0) {
       std::vector<double> nan(strategy->prediction_length(), NAN);
       std::vector<double> nan_error(1, NAN);
-      predictions.emplace_back(nan, nan, nan_error, nan_error);
+      predictions.emplace_back(nan, estimate_variance ? nan : std::vector<double>(), nan_error, nan_error);
       continue;
     }
 
