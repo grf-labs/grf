efd649f8779358a6fddbd45ef52e6023b37c7800
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-03-31 10:55:22 -0700

    Remove ll_regression_forest sample.weight + cluster weight option note (#868)

--- r-package/grf/R/local_linear_forest.R
+++ r-package/grf/R/local_linear_forest.R
@@ -26,9 +26,7 @@
 #'  smallest cluster has K units, then when we sample a cluster during training, we only give a random
 #'  K elements of the cluster to the tree-growing procedure. When estimating average treatment effects,
 #'  each observation is given weight 1/cluster size, so that the total weight of each cluster is the
-#'  same. Note that, if this argument is FALSE, sample weights may also be directly adjusted via the
-#'  sample.weights argument. If this argument is TRUE, sample.weights must be set to NULL. Default is
-#'  FALSE.
+#'  same. Default is FALSE.
 #' @param sample.fraction Fraction of the data used to build each tree.
 #'                        Note: If honesty = TRUE, these subsamples will
 #'                        further be cut by a factor of honesty.fraction. Default is 0.5.
