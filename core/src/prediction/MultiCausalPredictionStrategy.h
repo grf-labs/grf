a27c451f90f46b511465db656077c585d9a03d25
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-05-13 17:14:52 -0700

    Use `@link` in header doc

--- core/src/prediction/MultiCausalPredictionStrategy.h
+++ core/src/prediction/MultiCausalPredictionStrategy.h
@@ -27,7 +27,7 @@
 namespace grf {
 
 /**
- * This prediction strategy is a special case of {@link InstrumentalRelabelingStrategy}
+ * This prediction strategy is an extension of {@link InstrumentalPredictionStrategy}
  * with a multivariate treatment = instrument, and a vector-valued outcome Y.
  *
  * We estimate a forest weighted (alpha) estimate of beta in
