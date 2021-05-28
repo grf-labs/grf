0a5bf2864bb12a3ab76b5a13625383beeb4b58d4
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-05-28 08:39:44 -0700

    SE name

--- experiments/software_paper/table_regression.R
+++ experiments/software_paper/table_regression.R
@@ -13,16 +13,15 @@ tab.agg = aggregate(list(mse = df$mse, time = df$elapsed.sec),
                               dgp = df$dgp,
                               estimator = df$estimator),
                     FUN = mean)
-tab.agg.mse = aggregate(list(mse.se = df$mse, time.se = df$elapsed.sec),
+tab.agg.se = aggregate(list(mse.se = df$mse, time.se = df$elapsed.sec),
                              by = list(n = df$n,
                                        p = df$p,
                                        n.test = df$n.test,
                                        dgp = df$dgp,
                                        estimator = df$estimator),
                              FUN = function(x) sd(x) / sqrt(length(x)))
-
-tab.agg$mse = paste0(format(round(tab.agg$mse, 1), nsmall=1), " (", format(round(tab.agg.mse$mse.se, 2), nsmall=2), ")")
-tab.agg$time = paste0(format(round(tab.agg$time, 1), nsmall=1), " (", format(round(tab.agg.mse$time.se, 2), nsmall=2), ")")
+tab.agg$mse = paste0(format(round(tab.agg$mse, 1), nsmall=1), " (", format(round(tab.agg.se$mse.se, 2), nsmall=2), ")")
+tab.agg$time = paste0(format(round(tab.agg$time, 1), nsmall=1), " (", format(round(tab.agg.se$time.se, 2), nsmall=2), ")")
 
 print(tab.agg, digits = 2)
 apply(df[c("n", "p", "n.test", "dgp", "estimator")], 2, unique)
--- experiments/software_paper/table_survival.R
+++ experiments/software_paper/table_survival.R
@@ -13,17 +13,16 @@ tab.agg = aggregate(list(mse = 100*df$mse, time = df$elapsed.sec, err = 100*df$e
                               dgp = df$dgp,
                               estimator = df$estimator),
                     FUN = mean)
-tab.agg.mse = aggregate(list(mse.se = 100*df$mse, time.se = df$elapsed.sec, err.se = 100*df$err),
+tab.agg.se = aggregate(list(mse.se = 100*df$mse, time.se = df$elapsed.sec, err.se = 100*df$err),
                               by = list(n = df$n,
                                         p = df$p,
                                         n.test = df$n.test,
                                         dgp = df$dgp,
                                         estimator = df$estimator),
                               FUN = function(x) sd(x) / sqrt(length(x)))
-
-tab.agg$mse = paste0(format(round(tab.agg$mse, 1), nsmall=1), " (", format(round(tab.agg.mse$mse.se, 2), nsmall=2), ")")
-tab.agg$time = paste0(format(round(tab.agg$time, 1), nsmall=1), " (", format(round(tab.agg.mse$time.se, 2), nsmall=2), ")")
-tab.agg$err = paste0(format(round(tab.agg$err, 1), nsmall=1), " (", format(round(tab.agg.mse$err.se, 2), nsmall=2), ")")
+tab.agg$mse = paste0(format(round(tab.agg$mse, 1), nsmall=1), " (", format(round(tab.agg.se$mse.se, 2), nsmall=2), ")")
+tab.agg$time = paste0(format(round(tab.agg$time, 1), nsmall=1), " (", format(round(tab.agg.se$time.se, 2), nsmall=2), ")")
+tab.agg$err = paste0(format(round(tab.agg$err, 1), nsmall=1), " (", format(round(tab.agg.se$err.se, 2), nsmall=2), ")")
 
 print(tab.agg, digits = 2)
 apply(df[c("n", "p", "n.test", "dgp", "estimator")], 2, unique)
