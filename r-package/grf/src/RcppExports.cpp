e1bc50fa98032862e9662c2b46f870f1df97d2cd
Erik Sverdrup <erik.sverdrup@gmail.com>
2021-07-14 22:21:36 -0700

    Commit latest Rcpp 1.0.7 compile attributes

--- r-package/grf/src/RcppExports.cpp
+++ r-package/grf/src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // compute_split_frequencies
 Rcpp::NumericMatrix compute_split_frequencies(const Rcpp::List& forest_object, size_t max_depth);
 RcppExport SEXP _grf_compute_split_frequencies(SEXP forest_objectSEXP, SEXP max_depthSEXP) {
