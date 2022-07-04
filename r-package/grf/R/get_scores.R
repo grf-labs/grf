7ee28839565b982f192fd380b64fc08909b6cc9e
Erik Sverdrup <erik.sverdrup@gmail.com>
2022-07-03 18:50:14 -0700

    Simplify CSF DR score (#1177)

--- r-package/grf/R/get_scores.R
+++ r-package/grf/R/get_scores.R
@@ -315,12 +315,11 @@ get_scores.causal_survival_forest <- function(forest,
   }
 
   eta <- forest[["_eta"]]
-  numerator.one <- eta$numerator.one[subset]
-  numerator.two <- eta$numerator.two[subset]
+  numerator <- eta$numerator[subset]
+  denominator <- eta$denominator[subset]
   W.hat <- forest$W.hat[subset]
-  W.orig <- forest$W.orig[subset]
   cate.hat <- predict(forest)$predictions[subset]
-  correction <- numerator.one - numerator.two - cate.hat * (W.orig - W.hat)^2
+  psi <- numerator - denominator * cate.hat
 
-  cate.hat + correction * 1 / W.hat / (1 - W.hat)
+  cate.hat + psi * 1 / W.hat / (1 - W.hat)
 }
