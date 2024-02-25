df568ebe988dabe73d93bc95495e19e0cb270b18
Erik <erik.sverdrup@gmail.com>
2024-02-24 16:22:45 -0800

    Mention newsletter overview in CSF docstring

--- r-package/grf/R/causal_survival_forest.R
+++ r-package/grf/R/causal_survival_forest.R
@@ -94,8 +94,13 @@
 #' @return A trained causal_survival_forest forest object.
 #'
 #' @references Cui, Yifan, Michael R. Kosorok, Erik Sverdrup, Stefan Wager, and Ruoqing Zhu.
-#'  "Estimating Heterogeneous Treatment Effects with Right-Censored Data via Causal Survival Forests."
+#'  "Estimating Heterogeneous Treatment Effects with Right-Censored Data via Causal Survival Forests".
 #'  Journal of the Royal Statistical Society: Series B, 85(2), 2023.
+#'
+#' @references Sverdrup, Erik, and Stefan Wager.
+#'  "Treatment Heterogeneity with Right-Censored Outcomes Using grf".
+#'  ASA Lifetime Data Science Newsletter, January 2024
+#'  (\href{https://arxiv.org/abs/2312.02482}{arXiv:2312.02482}).
 #'
 #' @examples
 #' \donttest{
