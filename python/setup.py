ddd2919f8c6d621c7fd1d0c33fe2ca2f56428ae4
Apoorva Lal <lal.apoorva@gmail.com>
2024-07-07 13:29:38 -0700

    pybind11 setup

---
+++ python/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+import os
+
+# Get a list of all .cpp files in the core/src directory and its subdirectories
+grf_src_dir = "../core/src"
+grf_sources = []
+for root, dirs, files in os.walk(grf_src_dir):
+    for file in files:
+        if file.endswith(".cpp"):
+            grf_sources.append(os.path.join(root, file))
+
+ext_modules = [
+    Pybind11Extension(
+        "grf._grf_python",
+        ["grf/_grf_python.cpp", "grf/PyUtilities.cpp"] + grf_sources,
+        include_dirs=[
+            "../core/src",
+            "../core/third_party",
+            ".",
+        ],
+        extra_compile_args=["-std=c++17", "-fvisibility=hidden", "-g0"],
+        language="c++",
+    ),
+]
+
+setup(
+    name="grf",
+    version="0.1",
+    author="Apoorva Lal",
+    description="Python bindings for GRF",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    packages=["grf"],
+)
