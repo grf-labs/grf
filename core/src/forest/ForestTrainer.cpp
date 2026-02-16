7b5f831cbb42df4ccffdd591552e6a0e4c1d42c5
Erik <erik.sverdrup@gmail.com>
2026-02-16 14:28:08 -0800

    same pattern in trainer

--- core/src/forest/ForestTrainer.cpp
+++ core/src/forest/ForestTrainer.cpp
@@ -91,16 +91,37 @@ std::vector<std::unique_ptr<Tree>> ForestTrainer::train_trees(const Data& data,
                                  std::ref(user_interrupt_flag)));
   }
 
-  for (auto& future : futures) {
-    while (future.wait_for(std::chrono::milliseconds(100)) != std::future_status::ready) {
-      try {
-        grf::runtime_context.interrupt_handler();
-        progress_bar.update();
-      } catch (...) {
-        user_interrupt_flag = true;
-        throw;
+  // Periodically check for user interrupts + update progress bar while threads are working.
+  bool working = true;
+  while (working) {
+    try {
+      grf::runtime_context.interrupt_handler();
+      progress_bar.update();
+    } catch (...) {
+      user_interrupt_flag = true;
+      // Adhere to good C++ hygiene and clean up the futures before rethrowing
+      for (auto& future : futures) {
+        if (future.valid()) {
+          try { future.get(); } catch (...) {}
+        }
+      }
+      throw;
+    }
+    // Check if we can stop working
+    working = false;
+    for (const auto& future : futures) {
+      if (future.wait_for(std::chrono::milliseconds(0)) != std::future_status::ready) {
+        working = true;
+        break;
       }
     }
+    if (working) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+  }
+
+  // Collect the final results
+  for (auto& future : futures) {
     std::vector<std::unique_ptr<Tree>> thread_trees = future.get();
     trees.insert(trees.end(),
                  std::make_move_iterator(thread_trees.begin()),
