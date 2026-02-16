15ccd749783d4a6bff21a73ac7c0055b279c5c85
Erik <erik.sverdrup@gmail.com>
2026-02-15 22:38:52 -0800

    Revert "register R's interrupt handler"
    This reverts commit 2cb2c247ca6523e2dc16428e4c8aaa405d70ea6c.

--- r-package/grf/bindings/RcppUtilities.cpp
+++ r-package/grf/bindings/RcppUtilities.cpp
@@ -25,11 +25,6 @@
 
 using namespace grf;
 
-// [[Rcpp::init]]
-void init_grf_interrupt_handler(DllInfo* dll) {
-  grf::runtime_context.interrupt_handler = []() { Rcpp::checkUserInterrupt(); };
-}
-
 Rcpp::List RcppUtilities::create_forest_object(Forest& forest,
                                                const std::vector<Prediction>& predictions) {
   Rcpp::List result = serialize_forest(forest);
